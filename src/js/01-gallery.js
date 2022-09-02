c8e687d9a5d3387d71c491203bc81a478c07191f
Oleksii Vykhryst <19leshy77@gmail.com>
2022-09-02 23:01:42 +0300

    сделал первую часть

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const containerForGallery = document.querySelector('.gallery');
+const galleryMarkap = makeGallery(galleryItems);
+containerForGallery.insertAdjacentHTML('afterbegin', galleryMarkap);
+
+function makeGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
