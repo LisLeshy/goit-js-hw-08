aadf0d7a5bc8401cf6b39de5ae2863af84ae8e94
Oleksii Vykhryst <19leshy77@gmail.com>
2022-09-05 11:56:43 +0300

    исправил 2 задачку

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,54 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// шаблон
+// const onPlay = function (data) {
+//   // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
+
+const onPlay = function (data = {}) {
+  console.log(data.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+// шаблон
+// player
+//   .setCurrentTime(30.456)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onMessageInput, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+
+populateMessage();
+
+// 1
+const formData = {};
+function onMessageInput(e) {
+  //   console.log(e.target.value);
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+// 2
+
+function populateMessage() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parseMessage = JSON.parse(savedMessage);
+  if (savedMessage) {
+    textarea.value = parseMessage.message;
+    input.value = parseMessage.email;
+  }
+}
+
+// 3
+function onFormSubmit(e) {
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  e.preventDefault();
+  e.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
