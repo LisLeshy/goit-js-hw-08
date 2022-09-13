d3348767d561077e74a3e5bb2b2372ac0827515d
Oleksii Vykhryst <19leshy77@gmail.com>
2022-09-13 11:45:09 +0300

    исправил ошибку

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,53 +1,47 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const input = document.querySelector('.feedback-form input');
-const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onMessageInput, 500));
 
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+let formData = {};
 
 populateMessage();
 
-// 1
-
 function onMessageInput(e) {
-  //   console.log(e.target.value);
   formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-// 2
-
 function populateMessage() {
   let savedMessage = localStorage.getItem(STORAGE_KEY);
-  // console.log(savedMessage);
-  // 1 вытащил строку
+
   if (savedMessage) {
     const parseMessage = JSON.parse(savedMessage);
-    // console.log(parseMessage);
-    //2 распарсил( сделал объект)
+
     Object.entries(parseMessage).forEach(([name, value]) => {
-      //4 достал значение ключа (key) и поменять на вхождение (entries )
-      console.log(name, value);
-      // 5 деструкторизация значений
       formData[name] = value;
       form.elements[name].value = value;
-      // console.log(form.elements[name].value);
-      // присвоил значения
     });
-    // console.log(Object.keys(parseMessage));
+    console.log(Object.keys(parseMessage));
     //3 достал значения из объекта
   }
 }
 
 // 3
 function onFormSubmit(e) {
+  if (input.value === '' || textarea.value === '') {
+    window.alert('please fill in all input fields');
+    return;
+  }
+
   console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
   e.preventDefault();
   e.target.reset();
   localStorage.removeItem(STORAGE_KEY);
+  formData = {};
 }
